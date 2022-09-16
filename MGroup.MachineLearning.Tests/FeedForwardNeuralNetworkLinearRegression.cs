f60afc81b2e4952315d5466fa36c36be89d36282
StefPyr93 <stefpyr@gmail.com>
2022-09-16 10:39:31 +0300

    Add project files.

---
+++ MGroup.MachineLearning.Tests/FeedForwardNeuralNetworkLinearRegression.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Tensorflow;
+using Tensorflow.Keras;
+using Tensorflow.Keras.ArgsDefinition;
+using Tensorflow.Keras.Engine;
+using Tensorflow.NumPy;
+using Xunit;
+using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
+
+namespace MGroup.MachineLearning.Tests
+{
+    public class FeedForwardNeuralNetworkLinearRegression
+    {
+        private static void RunTest()
+        {
+            double[] trainX = {3.3f, 4.4f, 5.5f, 6.71f, 6.93f, 4.168f, 9.779f, 6.182f,
+                    7.59f, 2.167f, 7.042f, 10.791f, 5.313f, 7.997f, 5.654f, 9.27f, 3.1f };
+
+            double[] trainY = { 1.7f, 2.76f, 2.09f, 3.19f, 1.694f, 1.573f, 3.366f,
+                    2.596f, 2.53f, 1.221f, 2.827f, 3.465f, 1.65f, 2.904f, 2.42f, 2.94f, 1.3f };
+
+            double[] testX = { };
+
+            double[] testY = { };
+
+            var neuralNetwork = new FeedForwardNeuralNetwork();
+
+            neuralNetwork.NumHiddenLayers = 1;
+            neuralNetwork.NumNeuronsPerLayer = new int[] { 10 };
+            neuralNetwork.Optimizer = "SGD";
+            neuralNetwork.LearningRate = 0.01;
+            neuralNetwork.BatchSize = 64;
+            
+
+            Activation[] activationFunctionPerLayer = { keras.activations.Linear };
+
+            float learning_rate = 0.1f;
+            int display_step = 100;
+            int batch_size = 256;
+            int training_steps = 1000;
+        }
+    }
+}
---
+++ MGroup.MachineLearning/FeedForwardNeuralNetwork.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tensorflow;
+using Tensorflow.Keras;
+using Tensorflow.Keras.ArgsDefinition;
+using Tensorflow.Keras.Engine;
+using Tensorflow.NumPy;
+using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
+
+namespace MGroup.MachineLearning
+{
+    public class FeedForwardNeuralNetwork
+    {
+        string Preprocessing { get; set; }
+
+        //Optimization Parameters
+        public string Optimizer { get; set; }
+        public double LearningRate { get; set; }
+        public int BatchSize { get; set; }
+        public int Epochs { get; set; }
+        public int DisplayStep { get; set; }
+
+        //Neural Network Architecture
+        public int NumHiddenLayers { get; set;}
+        public int[] NumNeuronsPerLayer { get; set; }
+        public Activation[] ActivationFunctionPerLayer { get; set; }
+
+        public FeedForwardNeuralNetwork()
+        {
+            InitializeParameters();
+        }
+
+        private void InitializeParameters()
+        {
+            Optimizer = "SGD";
+            LearningRate = 0.01;
+            BatchSize = 64;
+            Epochs = 100;
+            NumHiddenLayers = 1;
+            NumNeuronsPerLayer = new int[] { 10 };
+        }
+
+        public void TrainNetwork()
+        {
+            tf.enable_eager_execution();
+
+            var optimizer = keras.optimizers.SGD((float)LearningRate);
+            
+        }    
+
+    }
+
+}
